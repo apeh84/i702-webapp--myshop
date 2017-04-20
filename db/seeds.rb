4c6a177fbae0c7b2525045129737ac761184cd49
Gabriel Apeh Adoyi <gabriel.adoyi@itcollege.ee>
2017-04-20 16:18:48 +0300

    initial commit add first functionality

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+Product.create! id: 1, name: "Shoe", price: 45.49, active: true
+Product.create! id: 2, name: "Boot", price: 57.29, active: true
+Product.create! id: 3, name: "jeans", price: 30.99, active: true
+
+OrderStatus.delete_all
+OrderStatus.create! id: 1, name: "Processing"
+OrderStatus.create! id: 2, name: "Placed"
+OrderStatus.create! id: 3, name: "Shipped"
+OrderStatus.create! id: 4, name: "Cancelled"
